8b4a8939ee3e5b97614fc9a11e25a2d1556362d1
infinityavoid <anton.ustinov02@mail.ru>
2022-11-12 12:59:15 +0500

    first

---
+++ app/components/Home.vue
@@ -0,0 +1,182 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Home"/>
+        </ActionBar>
+        <StackLayout>
+            <StackLayout>
+                <Button text = "->" @tap = "Next()" />
+                <Button text = "<-" @tap = "Back()" />
+            </StackLayout>
+                <StackLayout class="stack" v-for="Name,index in monthsNames" :key="index" v-if="Name === Months.Name">
+                    <label>{{Name}}</label>
+                </StackLayout>
+                <WrapLayout>
+                    <Label class="q" v-for="day in 7" :day="day" v-if="fnfirstweek(day)">{{Count}}</Label>
+                    <Label class="w" v-else>{{" "}}</Label>
+                    <Label :text = "d.day" class="e" v-for="d in monthdays">{{d.day}}</Label>
+                </WrapLayout>
+                <FlexboxLayout>
+                    <FlexboxLayout v-for="(d,index) in monthdays" :key = "index">
+                        <Label :text="d.day" textAlignment = "center"/>
+                    </FlexboxLayout>
+                </FlexboxLayout>
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+
+  export default {
+    data() {
+        return {
+            monthsNames: ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],
+            Days: [1, 2, 3, 4, 5, 6, 0],
+            Data: new Date(),
+            Months:               {   
+                    Name: '', //название месяца
+                    Number: '', //номер месяца начиная с 0
+                    NOD: '', //кол-во дней в месяце
+                    Year: '' //год 
+                } ,
+            Count: 0,
+            monthdays:[],
+        };
+    },
+    
+    computed:{
+    },
+    methods:{
+        calculatemd()
+        {
+            this.monthdays = []
+            for (let i = 1; i < this.Months.NOD + 1; ++i)
+            {
+                this.monthdays.push({day:i})
+            }
+            console.log(this.monthdays)
+        },
+        fnday(day)
+        {
+            let num = new Date(this.Months.Year,this.Months.Number,day).getDay()
+            return num 
+        },
+        fnfirstweek(day) //функция должна помочь вывести первую неделю в месяце, а затем в зависимости от Count выведутся последующие дни
+        {
+            let status = null
+            for(let i = 1; i < day+1;++i)
+            {
+               if(this.Days[day - 1] === this.fnday(i))
+               {
+                status = true
+                this.Count++
+                break
+               }
+               else
+               {
+                status = false
+               }
+            }
+
+            return status
+        },
+        fnmonthday(y,m)
+        {
+            let name = ((new Date(y,m).toLocaleDateString('default', {month:'short'})).trim().split(" "))[1]
+            return name
+        },
+        fnNOD(y,m)
+        {
+            let nod = new Date(y,m + 1,0).getDate()
+            return nod
+        },
+        Next()
+        {
+            if (this.Months.Number == 11)
+            {
+                this.Months.Number = 0
+                this.Months.Year++
+                this.Months.Name = this.fnmonthday(this.Months.Year,this.Months.Number)
+                this.Months.NOD = this.fnNOD(this.Months.Year,this.Months.Number)
+            }
+            else
+            {
+                this.Months.Number++
+                this.Months.Name = this.fnmonthday(this.Months.Year,this.Months.Number)
+                this.Months.NOD = this.fnNOD(this.Months.Year,this.Months.Number)
+            }
+            this.calculatemd()
+            console.log(this.Months) //можно увидеть что данные в объекте меняются
+        },
+        Back()
+        {
+            if (this.Months.Number == 0)
+            {
+                this.Months.Number = 11
+                this.Months.Year--
+                this.Months.Name = this.fnmonthday(this.Months.Year,this.Months.Number)
+                this.Months.NOD = this.fnNOD(this.Months.Year,this.Months.Number)
+            }
+            else
+            {
+                this.Months.Number--
+                this.Months.Name = this.fnmonthday(this.Months.Year,this.Months.Number)
+                this.Months.NOD = this.fnNOD(this.Months.Year,this.Months.Number)
+            }
+            console.log(this.Months)
+            this.calculatemd()
+        },
+    },
+    created() //задаются значения сегодняшней даты
+    {
+        this.Day = this.Data.getDate()
+        this.Month = this.Data.getMonth()
+        this.Year = this.Data.getFullYear()
+        this.Months.Number = new Date(this.Year,this.Month).getMonth(),
+        this.Months.Year = new Date(this.Year,this.Month,1).getFullYear()
+        this.Months.Name = this.fnmonthday(this.Year,this.Month)
+        this.Months.NOD = this.fnNOD(this.Year,this.Month)
+        console.log(this.Months.NOD)
+        this.calculatemd()
+        console.log(this.Months)
+    }
+};
+</script>
+
+<style>
+.w
+{
+    background-color: green;
+}
+.q
+{
+    background-color: purple;
+}
+.e
+{
+background-color: brown;
+}
+.flex
+{
+    width: 500px;
+    height: 60px;
+    background-color: black;
+}
+.stack
+{
+    width: 500px;
+    height: 100px;
+    background-color: blue;
+}
+    .label1
+    {
+        background-color: red;
+    }
+    .label2
+    {
+        background-color: green;
+    }
+
+
+
+</style>
